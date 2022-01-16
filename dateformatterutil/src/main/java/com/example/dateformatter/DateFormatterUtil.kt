6cb83fe026d5b8b9ade04b81bd854b30055af525
Ahmed Saber <saberahmed690@gmail.com>
2022-01-16 17:30:28 +0200

    solve github comments

--- dateformatterutil/src/main/java/com/example/dateformatter/DateFormatterUtil.kt
+++ dateformatterutil/src/main/java/com/example/dateformatter/DateFormatterUtil.kt
@@ -1,5 +1,6 @@
 package com.example.dateformatter
 
+import android.icu.util.LocaleData
 import org.joda.time.*
 import org.joda.time.chrono.GregorianChronology
 import org.joda.time.chrono.ISOChronology
@@ -7,14 +8,14 @@ import org.joda.time.chrono.IslamicChronology
 import java.text.ParseException
 import java.text.SimpleDateFormat
 import java.util.*
-
-
+/**
+ * @usage this object has many methods to deal with date formats
+*/
 object DateFormatterUtil {
-    //enum dateformat
     /**
-     * @usage this function is used to convert date of type string to date based on required date pattern
+     * @usage this function is used to parse string date to Date Object based on passed date format
      * @param from A String containing the date string
-     * @param outputDateFormatPattern A String represents required date format
+     * @param format A String represents the required format'
      * @return A Date value that represents the output date
      */
     fun convertStringToDate(from: String, format: String): Date? {
@@ -26,11 +27,10 @@ object DateFormatterUtil {
         return null
 
     }
-
     /**
-     * @usage this function is used to convert date of type Date to date String based on required date pattern
+     * @usage this function is used to format date of type Date to date String based on required date format
      * @param from A Date containing the date object
-     * @param format A String represents input date format
+     * @param format A String represents the required format
      * @return A String value that represents the output date
      */
     fun convertDateToString(from: Date, format: String): String {
@@ -48,16 +48,9 @@ object DateFormatterUtil {
      */
     fun convertToHijriDate(from: Date): LocalDate? {
         try {
-            val calendar = Calendar.getInstance()
-            calendar.timeInMillis = from.time
-            val iso: Chronology = GregorianChronology.getInstance()
+            val calendar = initCalendar(from)
             val hijri: Chronology = IslamicChronology.getInstance()
-
-            val todayIso = LocalDate(
-                calendar[Calendar.YEAR],
-                calendar[Calendar.MONTH] + 1,
-                calendar[Calendar.DAY_OF_MONTH], iso
-            )
+            val todayIso = initTodayIso(calendar)
             val todayHijri = LocalDate(
                 todayIso.toDateTimeAtStartOfDay(),
                 hijri
@@ -68,9 +61,33 @@ object DateFormatterUtil {
         }
         return null
     }
+    /**
+     * @usage this fun is used to init Today Iso from calender obj
+     * @param calendar Calendar object indicates date
+     * @return LocalDate object
+     */
+    fun initTodayIso(calendar:Calendar):LocalDate{
+        val iso: Chronology = GregorianChronology.getInstance()
+        return LocalDate(
+            calendar[Calendar.YEAR],
+            calendar[Calendar.MONTH] + 1,
+            calendar[Calendar.DAY_OF_MONTH]+1, iso
+        )
+    }
 
     /**
-     * @usage this function is used to convert LocalDateTime object to hijri date String
+     * @usage this fun is used to init calender time in millis from date
+     * @param from Date object indicates date
+     * @return Calender object
+     */
+    private fun initCalendar(from: Date): Calendar {
+        val calendar = Calendar.getInstance()
+        calendar.timeInMillis = from.time
+        return calendar
+    }
+
+    /**
+     * @usage this function is used to convert LocalDateTime object to date String
      * @param from A LocalDateTime containing the datetime object
      * @return A String that represents the date
      */
@@ -79,16 +96,14 @@ object DateFormatterUtil {
     }
 
     /**
-     * @usage this function is used to convert LocalDateTime object to hijri date String
+     * @usage this function is used to convert LocalDate object to hijri date String
      * @param localDate A LocalDate containing the date object
      * @param isIslamic A Boolean confirm that the date object
      * @return A Date that represents the localDate is islamic or normal date
      */
     fun convertFromIslamic(localDate: LocalDate, isIslamic: Boolean): Date? {
         return if (isIslamic) {
-            val iso: Chronology = ISOChronology.getInstanceUTC()
-            val dtIso = localDate.toDateTimeAtStartOfDay(DateTimeZone.UTC)
-                .withChronology(iso).toLocalDate()
+            val dtIso = initDateTimeIso(localDate)
             println("hijri-to-gregorian: $localDate=>$dtIso")
             dtIso.toDate()
         } else {
@@ -96,14 +111,24 @@ object DateFormatterUtil {
         }
     }
     /**
+     * @usage this function is used to init local date based on UTC time zone
+     * @param localDate A LocalDate object containing the date
+     * @return  LocalDate with UTC time Zone
+     */
+    fun initDateTimeIso(localDate: LocalDate):LocalDate{
+        val iso: Chronology = ISOChronology.getInstanceUTC()
+        return localDate.toDateTimeAtStartOfDay(DateTimeZone.UTC)
+            .withChronology(iso).toLocalDate()
+    }
+    /**
      * @usage this function is used to convert date to hours and minutes based on UTC time zone
      * @param date A String containing the date
      * @return A String that represents the hours and minutes
      */
-    fun convertDateToHoursMinUTC(dateString: String): String {
+    fun convertDateToHoursMinUTC(dateString: String,dateFormat: String="yyyy-MM-dd'T'HH:mm:ss"): String {
         val format = SimpleDateFormat("HH:mm")
         format.timeZone = TimeZone.getTimeZone("UTC")
-        val sdf = SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss")
+        val sdf = SimpleDateFormat(dateFormat)
         var d: Date? = null
         try {
             d = sdf.parse(dateString)
@@ -117,10 +142,10 @@ object DateFormatterUtil {
      * @param date A String containing the date
      * @return A String that represents the days and months
      */
-    fun convertDateToDateDaysMonthsUTC(date: String): String {
+    fun convertDateToDateDaysMonthsUTC(date: String,dateFormat: String="yyyy-MM-dd'T'HH:mm:ss"): String {
         val format = SimpleDateFormat("dd MMMM")
         format.timeZone = TimeZone.getTimeZone("UTC")
-        val sdf = SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss")
+        val sdf = SimpleDateFormat(dateFormat)
         var d: Date? = null
         try {
             d = sdf.parse(date)
@@ -130,44 +155,50 @@ object DateFormatterUtil {
         return format.format(d)
     }
     /**
-     * @usage this function is used to compare two date
+     * @usage this function is used to check to-date is after from-date
      * @param fromDate A String containing the start date
      * @param toDate A String containing the end date
      * @return A Boolean that indicates the end date is after start or not
      */
-    fun compareDates(fromDate: String , toDate: String): Boolean {
+    fun isFutureDate(fromDate: String , toDate: String): Boolean {
         return Date(toDate).after(Date(fromDate))
     }
     /**
-     * @usage this function is used to split hours and min from date based on am and pm
-     * @param date A String containing the date
+     * @usage this function is used to split hours and min from time based on am and pm
+     * @param time A String containing the date
      * @return A String that contains hh:min am/pm
      */
-    fun getDatFromPmAmHoursMin(date: String): String {
-        val h1 = date.split(":".toRegex()).toTypedArray()
-        val hour = h1[0]
-        val minute = h1[1]
-        return if (date.contains("PM")) "$hour:$minute PM" else "$hour:$minute AM"
+    fun getHoursMinFromTime(time: String): String {
+        if (time.contains(":")) {
+            val h1 = time.split(":".toRegex()).toTypedArray()
+            val hour = h1[0]
+            val minute = h1[1]
+            return if (time.contains("PM")) "$hour:$minute PM" else "$hour:$minute AM"
+        }
+        return "invalid input"
     }
     /**
      * @usage this function is used to get current date
+     * @param format A String that represents required format
      * @return A String that represents the current date
      */
-    fun getCurrentData(): String {
+    fun getCurrentData(format:String): String {
         val c = Calendar.getInstance().time
-        val df = SimpleDateFormat("yyyy/MM/dd", Locale.ENGLISH)
+        val df = SimpleDateFormat(format, Locale.ENGLISH)
         return df.format(c)
     }
     /**
-     * @usage this function is used to convert time from mill sec to sec
+     * @usage this function is used to get time from date String
      * @param georgianDate A String containing the date
+     * @param dateFormat A String containing the format of input date
+     * valid patterns : "yyyy/MM/dd - hh:mm:ss a" or "yyyy-MM-dd'T'HH:mm:ss"
      * @return A String that represents the sec of given date
      */
-    fun convertTimeFromMillSecondToSecond(georgianDate: String): String{
+    fun getHoursMinFromDate(georgianDate: String,dateFormat:String): String{
         try {
             val calendar = Calendar.getInstance()
             val df: java.text.DateFormat =
-                SimpleDateFormat("yyyy/MM/dd - hh:mm:ss a", Locale.ENGLISH)
+                SimpleDateFormat(dateFormat, Locale.ENGLISH)
             val dateLong = df.parse(georgianDate).time
             val formatter = SimpleDateFormat("hh:mm a", Locale.ENGLISH)
             calendar.timeInMillis = dateLong
--- dateformatterutil/src/test/java/com/example/dateformatter/DateFormatterUtilTest.kt
+++ dateformatterutil/src/test/java/com/example/dateformatter/DateFormatterUtilTest.kt
@@ -4,19 +4,19 @@ import org.joda.time.DateTimeZone
 import org.joda.time.LocalDate
 import org.joda.time.LocalDateTime
 import org.joda.time.tz.UTCProvider
-import org.junit.Assert.*
+import org.junit.Assert.assertEquals
 import org.junit.Before
 import org.junit.Test
-import java.util.*
 
 
-class DateFormatterUtilTest{
+class DateFormatterUtilTest {
 
     @Before
-    fun init(){
+    fun init() {
         //this line solve the problem : ZoneInfoMap not found
         DateTimeZone.setProvider(UTCProvider())
     }
+
     @Test
     fun convertStringToDate_return_valid_day_month_year_when_passing_valid_param() {
         //value returned is subtraction of 1900 according to documentation
@@ -57,58 +57,98 @@ class DateFormatterUtilTest{
     @Test
     fun convertToHijriDate() {
         assertEquals(
-            com.example.dateformatter.DateFormatterUtil.convertToHijriDate(
-                com.example.dateformatter.DateFormatterUtil.convertStringToDate(
+            DateFormatterUtil.convertToHijriDate(
+                DateFormatterUtil.convertStringToDate(
                     "12.01.2022 01:32:27",
                     "dd.MM.yyyy HH:mm:ss"
                 )!!
-            )?.year, LocalDate("1443-06-08").year
+            )?.dayOfMonth, LocalDate("1443-06-09").dayOfMonth
         )
     }
 
     @Test
-    fun convertLocalDateTimeToStringDate(){
+    fun convertLocalDateTimeToStringDate() {
         assertEquals(
-           com.example.dateformatter.DateFormatterUtil.convert(LocalDateTime.fromDateFields(
-               com.example.dateformatter.DateFormatterUtil.convertStringToDate(
-               "12.01.2022 01:32:27",
-               "dd.MM.yyyy HH:mm:ss"
-           )),"dd.MM.yyyy"),
+            DateFormatterUtil.convert(
+                LocalDateTime.fromDateFields(
+                    DateFormatterUtil.convertStringToDate(
+                        "12.01.2022 01:32:27",
+                        "dd.MM.yyyy HH:mm:ss"
+                    )
+                ), "dd.MM.yyyy"
+            ),
             "12.01.2022"
         )
     }
 
     @Test
-    fun convertHigriLocalDateToLocalDate(){
+    fun convertHigriLocalDateToLocalDate() {
         val date = com.example.dateformatter.DateFormatterUtil.convertStringToDate(
             "12-01-2022",
             "dd-MM-yyyy"
         )
         println(date.toString())
         val hijri = com.example.dateformatter.DateFormatterUtil.convertToHijriDate(date!!)
-        assertEquals(LocalDate("2022-01-12").toDate(),
-            com.example.dateformatter.DateFormatterUtil.convertFromIslamic(hijri!!,true))
+        assertEquals(
+            LocalDate("2022-01-12").toDate(),
+            com.example.dateformatter.DateFormatterUtil.convertFromIslamic(hijri!!, true)
+        )
     }
+
     @Test
-    fun convertDateToHoursMinUTC(){
-        assertEquals("10:17", com.example.dateformatter.DateFormatterUtil.convertDateToHoursMinUTC("2022-01-13T12:17:00"))
+    fun convertDateToHoursMinUTC() {
+        assertEquals(
+            "10:17",
+            com.example.dateformatter.DateFormatterUtil.convertDateToHoursMinUTC("2022-01-13T12:17:00")
+        )
     }
+
     @Test
-    fun convertDateToDateDaysMonthsUTC(){
-        assertEquals("13 January", com.example.dateformatter.DateFormatterUtil.convertDateToDateDaysMonthsUTC("2022-01-13T12:17:00"))
+    fun convertDateToDateDaysMonthsUTC() {
+        assertEquals(
+            "13 January",
+            com.example.dateformatter.DateFormatterUtil.convertDateToDateDaysMonthsUTC("2022-01-13T12:17:00")
+        )
     }
+
     @Test
-    fun compareDates(){
-        assertEquals(true, com.example.dateformatter.DateFormatterUtil.compareDates("2022/01/13","2022/01/14"))
+    fun compareDates() {
+        assertEquals(
+            true,
+            com.example.dateformatter.DateFormatterUtil.isFutureDate("2022/01/13", "2022/01/14")
+        )
     }
+
     @Test
-    fun getDatFromPmAmHoursMin(){
-        assertEquals("22:17 PM", com.example.dateformatter.DateFormatterUtil. getDatFromPmAmHoursMin("22:17:00 PM"))
-        assertEquals("02:17 AM", com.example.dateformatter.DateFormatterUtil. getDatFromPmAmHoursMin("02:17:00 AM"))
+    fun getDatFromPmAmHoursMin() {
+        assertEquals(
+            "22:17 PM",
+            DateFormatterUtil.getHoursMinFromTime("22:17:00 PM")
+        )
+        assertEquals(
+            "02:17 AM",
+            DateFormatterUtil.getHoursMinFromTime("02:17:00 AM")
+        )
     }
+
     @Test
-    fun getCurrentDate(){
-        assertEquals("2022/01/13", com.example.dateformatter.DateFormatterUtil. getCurrentData())
+    fun getCurrentDate() {
+        assertEquals("16.01.2022", DateFormatterUtil.getCurrentData("dd.MM.yyyy"))
+    }
+
+    @Test
+    fun convertTimeFromMillSecondToSecond() {
+        assertEquals(
+            "09:45 AM",
+            DateFormatterUtil.getHoursMinFromDate("2012-10-01T09:45:00", "yyyy-MM-dd'T'HH:mm:ss")
+        )
+        assertEquals(
+            "09:45 AM",
+            DateFormatterUtil.getHoursMinFromDate(
+                "2012/10/01 - 09:45:00 AM",
+                "yyyy/MM/dd - hh:mm:ss a"
+            )
+        )
     }
 
 }
